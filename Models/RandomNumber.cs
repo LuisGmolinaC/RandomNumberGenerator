67035bb981ddaed7f3f2290f777f43e9526962bc
LuisGmolinaC <xdevnon@gmail.com>
2024-09-11 13:19:48 -0500

    Enhance program with random number generation feature
    Removed initial comment and "Hello, World!" message in `Program.cs`.
    Added `using` directive for `RandomNumberGenerator.Models`.
    Updated main program to welcome user and handle input.
    Added `GenerateRandomNumber()` method for input handling and number generation.
    Implemented key press logic for generating numbers or exiting.
    Added `RandomNumber` class in `RandomNumber.cs` to generate and display random numbers.

---
+++ Models/RandomNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumberGenerator.Models
+{
+    public class RandomNumber
+    {
+        public int random;
+        public RandomNumber()
+        {
+            Random randomNumber = new Random();
+            int random = randomNumber.Next(1, 100);
+
+            Console.WriteLine(random);
+
+
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RandomNumberGenerator.Models;
+
+Console.WriteLine("Welcome to my Random Number Generator!");
+Console.WriteLine("Please press the key 'Enter' to generate a new number or the 'space' bar to end the program");
+GenerateRandomNumber();
+
+
+static void GenerateRandomNumber()
+{
+    var key = Console.ReadKey().Key;
+
+    if (key == ConsoleKey.Enter)
+    {
+        Console.WriteLine("\nHere is your random number!");
+        RandomNumber randomNumber = new RandomNumber();
+        Console.WriteLine("Press 'Enter' to generate another number or 'Space' to exit.");
+        GenerateRandomNumber(); // Llamada recursiva
+    }
+    else if (key == ConsoleKey.Spacebar)
+    {
+        Console.WriteLine("\nGoodbye!");
+    }
+    else
+    {
+        Console.WriteLine("\nInvalid input. Please try again.");
+        GenerateRandomNumber(); // Llamada recursiva en caso de entrada inválida
+    }
+}
